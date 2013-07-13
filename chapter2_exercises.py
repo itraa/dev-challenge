a823a5ae549c51c69f50ab636bed6ea14f933ce0
itraa <itraa.narayan@gmail.com>
2013-07-13 12:51:22 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,19 @@
 # Exercises for chapter 2:
-
-# Name:
+# Problem 2.1:  The leading 0 indicates to Python that it is octal and since 02492 has a 9, which is not a valid octal representation. The others numbers in the example work because they are comverted to octal number and do not have a 8 or a 9.
+# Problem 2.2:  Script below
+print 5
+x = 5
+print x + 1
+#--------------------------------------------------
+# Problem 2.3:
+# 1. Value =8, Type = int
+# 2. Value = 8.5, Type = float
+# 3. Value = 4.0, Type = float
+# 4. Value = 11, Type = int
+# 5. Value = '.....', Type = str
+#--------------------------------------------------
+#  Problem 2.4:  
+# 1. Volume is 523 cubic unit
+# 2. Cost is $945.45
+# 3. Time = 7:30:06am 
+# Name: Aarthi Narayan
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,42 @@
 # Exercises for chapter 3:
+# Problem 3.1: Since the function was not defined before the call the following error occured while execution
+# Traceback (most recent call last):
+#  File "problem31.py", line 1, in <module>
+#    repeat_lyrics()
+#  NameError: name 'repeat_lyrics' is not defined
+#-------------------------------------------------------------
+#  Problem 3.2: Sucessfully executes with following display
+# I'm a lumberjack and I'm okay.
+# I sleep all night and I work all day.
+# I'm a lumberjack and I'm okay.
+# I sleep all night and I work all day.
+#--------------------------------------------------------------
+#  Problem 3.3: 
+def right_justify(s):
+    y = 70 - len('s')
+    print ' ' * y + s
 
-# Name:
+right_justify('allen')
+#---------------------------------------------------------------
+#  Problem 3.4:
+# 1. Displays the following:
+#spam
+#spam
+# 2. 3.  4. 5. Code below:
+def do_twice(f,g):
+    f(g)
+    f(g)
+
+def print_twice(g):
+    print g
+    print g
+
+def do_four(f,g):
+    do_twice(f,g)
+    do_twice(f,g)
+
+do_twice(print_twice,'spam')
+
+do_four(print_twice,'spam')
+#-----------------------------------------------------------------------
+# Name:Aarthi Narayan
